eab43623b1ead605268260962c9774f7420c35c3
Danilo Kaltner <danilokaltner@Danilos-MacBook-Pro.local>
2020-11-23 08:59:19 -0800

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def shortestPath(a, b: int) -> int:
+    
+
+if __name__ == "__main__":
+    print shortestPath(0, 10)
---
+++ main_test.py
@@ -0,0 +1,3 @@
+class TestShortestPath:
+    def shortest_path(self):
+        assert shortestPath(0, 10) == 4
